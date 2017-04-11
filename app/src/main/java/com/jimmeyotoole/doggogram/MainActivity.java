a401524f6883356369ec3d16462b005eaab366ec
James O'Toole <jimmeyotoole@gmail.com>
2017-04-11 13:04:38 +1000

    img

--- app/src/main/java/com/jimmeyotoole/doggogram/MainActivity.java
+++ app/src/main/java/com/jimmeyotoole/doggogram/MainActivity.java
@@ -18,11 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         //Create dummy doggo data, to be replaced with a web call soon.
         ArrayList<Doggo> doggos = new ArrayList<Doggo>();
-        doggos.add(new Doggo("Bob",""));
-        doggos.add(new Doggo("Billy",""));
+        doggos.add(new Doggo("Charlie","http://nomossgames.com/uploadserver/uploads/1491539018pupwithshoelace.jpg"));
+        doggos.add(new Doggo("Sam",""));
 
         lv_main=(ListView)findViewById(R.id.mainlist);
         MainListViewAdapter mainAdapter = new MainListViewAdapter(this,R.layout.mainrowitem,doggos);
         lv_main.setAdapter(mainAdapter);
+
+        
     }
 }
